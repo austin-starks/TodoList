f3ca9e638bf28b930838edac170dd9d81cbd6500
Austin Starks <as2779@cornell.edu>
2020-01-14 16:14:57 -0500

    Add ability to rearrange and add todos

--- assets/javascript/todos.js
+++ assets/javascript/todos.js
@@ -1,12 +1,31 @@
 // Strikethrough lis when we click on it
-$("li").on("click", function () {
+$("ul").on("click", "li", function () {
     $(this).toggleClass("strikethrough");
 })
 
-// CLick on X to delete todo
-$(".deleteButton").on("click", function (event) {
+// Click on X to delete todo
+$("ul").on("click", ".deleteButton", function (event) {
+    // Stop the item from becoming greyed out strikethroughed
     event.stopPropagation();
-    $(this).parent().fadeOut(200, function () {
-        $(this).remove();
-    });
+    var result = confirm("Are you sure you want to delete this TODO?");
+    if (result) {
+        $(this).parent().fadeOut(200, function () {
+            $(this).remove();
+        });
+    }
+})
+
+// Rearrange lis when you click and drag it
+$(function () {
+    $("#sortable").sortable();
+    $("#sortable").disableSelection();
+});
+
+// Add additional todos to the list
+$("input[type = 'text']").on("keypress", function (event) {
+    if (event.which === 13) {
+        var newTodo = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span class = 'deleteButton'>X </span>" + newTodo + "</li>")
+    };
 })
